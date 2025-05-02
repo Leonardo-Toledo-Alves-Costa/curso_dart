cc4bd88eaafc3e0a31ad82431cbe3997783fb6b4
Leonardo Toledo <2025010842@unifei.edu.br>
2025-05-02 01:32:38 -0300

    Aula 479 - Variaveis 01

--- fundamentos/primeiro.dart
+++ fundamentos/primeiro.dart
@@ -1,3 +1,4 @@
-main () {
+main ()
+{
   print ('Olá, dart!');
 }
---
+++ fundamentos/variaveis_1.dart
@@ -0,0 +1,11 @@
+main() 
+{
+int numeroInteiro = 10; 
+double numeroDecimal = 10.5;
+String nome = 'Dart';
+bool verdadeiro = true;
+List lista = [1, 2, 3, 4, 5];
+print("Meu nome é $nome e minha idade é de $numeroInteiro anos.\n");
+print("Tenho $numeroDecimal metros de altura e isso é $verdadeiro.\n");
+print("Minha lista é: $lista\n");
+}
