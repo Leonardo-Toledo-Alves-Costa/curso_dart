1cff3b3fe339f2fca4db05cf37d232e6fc019e8b
Leonardo Toledo <2025010842@unifei.edu.br>
2025-05-02 01:45:24 -0300

    aula 480 - Variaveis 02

---
+++ fundamentos/variaveis_2.dart
@@ -0,0 +1,22 @@
+main()
+{
+  // Variáveis
+  var nome = 'Lucas';
+  var idade = 20;
+  var altura = 1.80;
+  var peso = 70.5;
+  var boleano = true;
+
+  // Exibindo os valores
+  print('Nome: $nome');
+  print('Tipo da variavel nome é: ' '${nome.runtimeType}');
+  print('Idade: $idade');
+  print('Tipo da variavel idade é: ' '${idade.runtimeType}');
+  print('Altura: $altura');
+  print('Tipo da variavel altura é: ' '${altura.runtimeType}');
+  print('Peso: $peso');
+  print('Tipo da variavel peso é: ' '${peso.runtimeType}');
+  print('Verdadeiro ou falso: $boleano');
+  print('Tipo da variavel boleano é: ' '${boleano.runtimeType}');
+
+}
